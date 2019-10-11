058d1260cf386c9bb29de46fdfbd72d69daa5973
jeffreyko87 <54325118+jeffreyko87@users.noreply.github.com>
2019-10-11 15:33:58 -0500

    week8

---
+++ p5/Week8oop1/sketch.js
@@ -0,0 +1,51 @@
+var myCar;
+var myCar1;
+var myCar2;
+
+function setup() {
+  createCanvas(800,800);
+  noStroke();
+  myCar=new Car();
+  myCar1=new Car();
+  myCar2=new Car();
+}
+
+function draw() {
+  background(100);
+
+  myCar.display();
+  myCar.drive();
+
+  myCar1.display();
+  myCar1.drive();
+
+  myCar2.display();
+  myCar2.drive();
+}
+function Car(){
+  //attributes
+  this.x=0;
+  this.y=random(height);
+  this.r=random(255);
+  this.g=random(255);
+  this.b=random(255);
+  this.vel=random(5);
+
+
+
+  //display method
+  this.display=function(){
+    fill(this.r,this.g,this.b);
+    rect(this.x,this.y,100,50);
+  }
+
+
+
+  this.drive=function(){
+    this.x=this.x+this.vel;
+    if(this.x>width){
+      this.x=0;
+
+    }
+  }
+}
